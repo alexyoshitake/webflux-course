c3dcc4c472caddf01fe2881cc2169f6ba3fdf404
Alex Yoshitake <alex.yoshitake@gmail.com>
2023-07-17 23:09:09 -0300

    start project

---
+++ src/main/java/br/com/yoshitake/webfluxcource/WebfluxCourceApplication.java
@@ -0,0 +1,13 @@
+package br.com.yoshitake.webfluxcource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCourceApplication.class, args);
+	}
+
+}
