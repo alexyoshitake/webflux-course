71a6fa1fdd74ebdf166642ed7c46059725cbcc10
Alex Yoshitake <alex.yoshitake@gmail.com>
2023-07-18 22:57:10 -0300

    add string validator

--- src/main/java/br/com/yoshitake/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/yoshitake/webfluxcourse/model/request/UserRequest.java
@@ -1,19 +1,23 @@
 package br.com.yoshitake.webfluxcourse.model.request;
 
+import br.com.yoshitake.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+		@TrimString
 		@Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
 		@NotBlank(message = "must not be null or empty")
 		String name,
 
+		@TrimString
 		@Email(message = "invalid email")
 		@NotBlank(message = "must not be null or empty")
 		String email,
 
+		@TrimString
 		@Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
 		@NotBlank(message = "must not be null or empty")
 		String password
